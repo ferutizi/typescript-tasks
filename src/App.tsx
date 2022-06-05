f3b860355378da8a34f0c56e0f74aaa3b884a649
ferutizi <fer.utizi@gmail.com>
2022-06-05 16:13:50 -0300

    map

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
+import { type } from "os";
 import React, { useState } from "react";
 
 type FormElement = React.FormEvent<HTMLFormElement>;
+interface ITask {
+  name: string;
+  done: boolean;
+}
 
 function App(): JSX.Element {
 
   const [task, setTask] = useState<string>('');
-  const [tasks, setTasks] = useState()
+
+  const [tasks, setTasks] = useState<ITask[]>([])
 
   const handleSubmit = (e: FormElement) => {
     e.preventDefault();
-    console.log(task);
+    addTask(task);
+    setTask('')
+  }
+
+  const addTask = (name: string) => {
+    const newTasks: ITask[] = [...tasks, {name, done: false}]
+    setTasks(newTasks)
+    console.log(tasks)
   }
 
   return (
     <>
       <h1>Typescript</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={e => setTask(e.target.value)}/>
+        <input type="text" onChange={e => setTask(e.target.value)} value={task}/>
         <button>save</button>
       </form>
+      {
+        tasks.map((item: ITask, i: number) => {
+          return <h1 key={i}>{item.name}</h1>
+        })
+      }
     </>
   );
 }
